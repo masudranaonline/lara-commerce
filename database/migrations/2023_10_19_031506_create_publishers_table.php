f779aecccc6cbd40eeb16596fe104ed6b81ec56e
Mehedi Jaman <mail4mjaman@gmail.com>
2023-10-19 09:50:59 +0600

    Feature: - Author - Publisher - Book - Book Deals - Category Migration refactor - products migration refactor

--- database/migrations/2023_10_19_031506_create_publishers_table.php
+++ database/migrations/2023_10_19_031506_create_publishers_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('publishers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('image')->nullable();
-            $table->text('description')->nullable();
+            $table->string('bio')->nullable();
+            $table->string('image', 500);
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('publishers');
     }
 };
--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -1,6 +1,11 @@
 @extends('./admin/layout/master')
 
 @section('content')
+    <style>
+        .button-group{
+            display: flex;
+        }
+    </style>
     <main id="main" class="main">
     <div class="card">
         <div class="card-body">
@@ -29,11 +34,17 @@
                 <td>{{ $product->sales_price }}</td>
                 <td>{{ $product->minimum_quantity  }}</td>
                 <td><img class="img-fluid" src="/admin/assets/img/aosomapto.gif" style="height: 44px; width: 45px;" alt=""></td>
-                <td>
+                <td class="button-group">
                   <a href="/admin-panel/product/show/{{ $product->id }}" class="btn btn-info">view details</a>
                   <a href="/admin-panel/product/edit/{{ $product->id }}" class="btn btn-warning">Edit</a>
 
-                  {{ Form::open(['url' => 'admin-panel/product/destroy/'.$product->id, 'method' => 'POST']) }}
+                  {{ Form::open([
+                    'url' => 'admin-panel/product/destroy/'.$product->id,
+                    'method' => 'POST',
+                    'class' => 'form-horizontal'
+                    ])
+                }}
+                    <button type="submit">icon</button>
                     <input type="submit" name="submit" value="Delete" class="btn btn-danger">
                   {{ Form::close() }}
                 </td>
