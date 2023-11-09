eb64b726844779b27e42cdea90587aa53fa4b0c9
Mehedi Jaman <mail4mjaman@gmail.com>
2023-11-09 09:30:00 +0600

    - Delete from wishlist after adding to cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -29,30 +29,9 @@ class CartController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request, $productId)
+    public function store($productId)
     {
-        $user_cart_item = cart::where('product_id', $productId)
-            ->where('user_id', Auth::id())
-            ->first();
-
-        $product = product::find($productId);
-
-        if($user_cart_item == null){
-            $cart  = new cart();
-
-            $cart->product_id = $productId;
-            $cart->user_id = Auth::id();
-            $cart->quantity = 1;
-            $cart->price = $product->sales_price;
-            $cart->total = $product->sales_price;
-
-            $cart->save();
-        }else{
-            $user_cart_item->quantity += 1;
-            $user_cart_item->price = $product->sales_price;
-            $user_cart_item->total = $user_cart_item->quantity * $user_cart_item->price;
-            $user_cart_item->save();
-        }
+        cart::store($productId);
         return back();
     }
 
